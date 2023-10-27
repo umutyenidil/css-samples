69f33603872854269e1e86f0bdb065ac6fc9975c
Umut Yenidil <umut.yenidil9@gmail.com>
2023-10-27 16:28:57 +0300

    sample-8 completed

--- sample-8/script.js
+++ sample-8/script.js
@@ -1,11 +1,19 @@
-let toggler = document.querySelector('.toggle');
-let topBar = document.querySelector('.top-bar');
-let navigation = document.querySelector('.navigation');
-let main = document.querySelector('.main');
+let toggler = document.querySelector(".toggle");
+let topBar = document.querySelector(".top-bar");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
 
-toggler.onclick = function(){
-    toggler.classList.toggle('active');
-    topBar.classList.toggle('active');
-    navigation.classList.toggle('active');
-    main.classList.toggle('active');
+toggler.onclick = function () {
+  toggler.classList.toggle("active");
+  topBar.classList.toggle("active");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
+
+function toggleMenu() {
+  let topBar = document.querySelector(".top-bar");
+  let main = document.querySelector(".main");
+
+  navigation.classList.remove('active');
+  main.classList.remove('active');
 }
